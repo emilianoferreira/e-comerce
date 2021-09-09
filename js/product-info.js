89493d6af413e39eb9aa75dc9778d657f26d5980
emilianoferreira <emilianoferreyra2001@gmail.com>
2021-09-09 08:24:47 -0300

    entrega 3.0

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,20 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+
+// entrega 3, mostrar hasta img, vinculado a products info 
+// en todos los productos se van a mostrar la misma data al ser un json
+// peticion y mostrar en pantalla la data del json 
+
+//parte 2. cargar products info url para mostrar toda la data de cada comentario del json 
+// Desafiate ponerle estrellas al score y mostrar el comentario mostrado  con inner+= para no perder lo que esta funcion hora data time o 
+
+document.addEventListener("DOMCsontentLoaded", function(e){
 
 });
+ 
+/*
+funcion para que dependiendo del puntaje el color de las estrellas if span<2 .class color rojo
+
+bootstrap 4.3 copiar y pegar los links dados en download en órdenes link en head y scripts al final de body
+jquery y popper también */
