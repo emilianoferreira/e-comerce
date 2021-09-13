c33c7d7398053914b96adb0f4c49b3b961f5280c
emilianoferreira <emilianoferreyra2001@gmail.com>
2021-09-13 17:03:39 -0300

    entrega 3.1

--- js/product-info.js
+++ js/product-info.js
@@ -8,11 +8,28 @@
 
 //parte 2. cargar products info url para mostrar toda la data de cada comentario del json 
 // Desafiate ponerle estrellas al score y mostrar el comentario mostrado  con inner+= para no perder lo que esta funcion hora data time o 
+const url = PRODUCT_INFO_URL;
 
-document.addEventListener("DOMCsontentLoaded", function(e){
+fetch(url)
+    .then(respuesta => respuesta.json())
+    .then(datos => {//del elemento insertar el dato del json
+        document.getElementById('name').innerHTML += datos.name;
+        document.getElementById('description').innerHTML += datos.description;
+        document.getElementById('cost').innerHTML += datos.cost;
+        document.getElementById('currency').innerHTML += datos.currency;
+        document.getElementById('soldCount').innerHTML += datos.soldCount;
+        document.getElementById('category').innerHTML += datos.category;
+        document.getElementById('images').innerHTML += datos.images + ".png";
+        document.getElementById('relatedProducts').innerHTML += datos.relatedProducts;
+    })
+    .catch(error => alert("Hubo un rerror: " + error));
+
+
+
+document.addEventListener("DOMCsontentLoaded", function (e) {
 
 });
- 
+
 /*
 funcion para que dependiendo del puntaje el color de las estrellas if span<2 .class color rojo
 
