a0d20a99b27bb85552379e3140333120c75071b8
emilianoferreira <emilianoferreyra2001@gmail.com>
2021-09-19 21:15:40 -0300

    3.5

---
+++ js/comments.js
@@ -0,0 +1,49 @@
+var comments = [];
+
+
+function getDate() {
+    let date = new Date();
+    let formatDate = date.getDate().toString().padStart(2, '0') + "/" + (date.getMonth() + 1).toString().padStart(2, '0') + "/" + date.getFullYear().toString();
+
+    return formatDate;
+}
+
+function saveComment() {
+    let comment = {
+        message: document.getElementById("textarea").value,
+        date: getDate(),
+        
+        user: localStorage.getItem("user")
+    }
+    comments.push(comment);
+    showComment();
+}
+
+function drawStars(stars) {
+    let number = parseInt(stars);
+    let html = "";
+    for (let i = 1; i <= number; i++) {
+        html += '<span class= "fa fa-star checked"></span>'
+    }
+    for (let k = number + 1; k <= 5; k++) {
+        html += '<span class= "fa fa-star"></span>'
+    }
+    return html;
+}
+
+function showComment() {
+    let html = ""
+    for (let i = comments.length - 1; i >= 0; i--) {
+        let comment = comments[i];
+        html +=
+            `<div class="bd-example">
+                <dl>
+                    <dt>${comment.user}-${comment.date}-${drawStars(comment.score)}</dt>
+                        <dd>${comment.message}</dd>
+                </dl>
+            </div>`
+    }
+document.getElementById("comentarios").innerHTML = html;
+//document.getElementById("formulario").reset();
+}
+
--- js/product-info.js
+++ js/product-info.js
@@ -19,6 +19,65 @@
 /*for (let i=1; imenorigual 5; i++){
     if (i menorigual score ) 
 }*/
+var product = {};
+var infoLista = [];
+function  showComment() {
+    // infoLista = sortComentariosByDate(comentariosLista);
+    for (let i = 0; i < infoLista.length; i++) {
+      let comentarios = infoLista[i];
+  console.log(comentarios)
+    //   for (let i = 1; i <= 5; i++) {
+    //     if (i <= comentarios.score) {
+    //       COMENTARIOS.innerHTML += `<span class="fas fa-star checked"></span>`
+    //     } else {
+    //       COMENTARIOS.innerHTML += `<span class="fas fa-star"></span>`
+    //     }
+    //   }
+  
+    //   COMENTARIOS.innerHTML += `<div class="row"><div class="col"><div class="d-flex w-100 justify-content-between">
+    //       <h6 class="font-weight-bold">${comentarios.user}</h6>
+    //       <small class="text-muted">${comentarios.dateTime}</small></div>
+    //       <p class="mb-3">${comentarios.description}</p></div></div>
+    //       <hr class="mt-0">`
+    // };
+    // FORMULARIO_COMENTARIOS.reset();
+  }};
+// function showComment() {
+//     let htmlContentToAppend = "";
+//     for (let i = 0; i < infoLista.length; i++) {
+//         let comentario = infoLista[i];
+
+
+//         htmlContentToAppend += `<p>${comentario.user}</p>`
+
+//     }
+
+//     document.getElementById('comentarios').innerHTML += htmlContentToAppend;
+// };
+
+// console.log(infoLista);
+
+
+/*fetch(PRODUCT_INFO_COMMENTS_URL)
+    .then(response => response.json())
+    .then(info => {
+        infoLista = info
+        showComment();
+        console.log(infoLista);
+
+        /* let commentScore = document.getElementById('score');
+         let commentDescription = document.getElementById('descriptionC');
+         let commentUser = document.getElementById('user');
+         let commentDateTime = document.getElementById('time');
+ 
+         commentScore.innerHTML += info.score;
+         commentDescription.innerHTML += info.description;
+         commentUser.innerHTML += info.user;
+         commentDateTime.innerHTML += info.dateTime;*/
+
+
+
+
 function showImagesGallery(array) {
 
     let htmlContentToAppend = "";
@@ -34,38 +93,72 @@ function showImagesGallery(array) {
         </div>
         `
 
-        document.getElementById("images").innerHTML = htmlContentToAppend;
+        document.getElementById("commentsImagesGallery").innerHTML = htmlContentToAppend;
     }
 }
 
-fetch(PRODUCT_INFO_URL)
+document.addEventListener("DOMContentLoaded", function (e) {
+
+    getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            product = resultObj.data;
+
+            let productNameHTML = document.getElementById('prodName');
+            let productDescriptionHTML = document.getElementById('prodDescription');
+            let prodCostHTML = document.getElementById('prodCost');
+            let productCurrrencyHTML = document.getElementById('prodCurrency');
+            let productSoldCountHTML = document.getElementById('soldCount')
+            let productCategoryHTML = document.getElementById('prodCategory');
+
+            productNameHTML.innerHTML += product.name;
+            productDescriptionHTML.innerHTML += product.description;
+            prodCostHTML.innerHTML += product.cost;
+            productCurrrencyHTML.innerHTML += product.currency
+            productSoldCountHTML.innerHTML += product.soldCount;
+            productCategoryHTML.innerHTML += product.category;
+
+
+            showImagesGallery(product.images);
+        }
+    });
+
+
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            infoLista = resultObj.data;
+
+            showComment();
+        }
+    });
+
+    // .catch(error => alert("Hubo un rerror: " + error));
+});
+
+
+/*fetch(PRODUCT_INFO_URL)
     .then(respuesta => respuesta.json())
     .then(datos => {//del elemento insertar el dato del json
-        document.getElementById('name').innerHTML += datos.name;
+        document.getElementById('ame').innerHTML += datos.name;
         document.getElementById('description').innerHTML += datos.description;
         document.getElementById('cost').innerHTML += datos.cost;
         document.getElementById('currency').innerHTML += datos.currency;
         document.getElementById('soldCount').innerHTML += datos.soldCount;
         document.getElementById('category').innerHTML += datos.category;
-        document.getElementById('images').innerHTML += datos.images;
     })
-    .catch(error => alert("Hubo un rerror: " + error));
+
+showImagesGallery(datos.images)
+
+    .catch(error => alert("Hubo un rerror: " + error));*/
 
 
-fetch(PRODUCT_INFO_COMMENTS_URL)
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('score').innerHTML += data.score;
-        document.getElementById('descriptionC').innerHTML += data.description;
-        document.getElementById('user').innerHTML += data.user;
-        document.getElementById('dateTime').innerHTML += data.dateTime;
-    })
-    .catch(error => alert("Hubo un rerror: " + error));
+/*document.getElementById('name').innerHTML += product.name;
+            document.getElementById('description').innerHTML += product.description;
+            document.getElementById('cost').innerHTML += product.cost;
+            document.getElementById('currency').innerHTML += product.currency;
+            document.getElementById('soldCount').innerHTML += product.soldCount;
+            document.getElementById('category').innerHTML += product.category;*/
 
-document.addEventListener("DOMCsontentLoaded", function (e) {
-    
-});
-
+            //Muestro las imagenes en forma de galería
 /*
 funcion para que dependiendo del puntaje el color de las estrellas if span<2 .class color rojo
 
